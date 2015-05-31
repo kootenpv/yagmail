019bfa629148ae2c04fe26e738219b66f2b1c23f
Pascal van Kooten <kootenpv@gmail.com>
2015-05-31 12:12:18 +0200

    trying to fix setup.py also trying to run setup.py locally

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ from setuptools import setup
 import sh
 # run updater like: python setup.py sdist bdist_wininst upload
 
-MAJOR_VERSION = '0'
-MINOR_VERSION = '3'
-MICRO_VERSION = sh.git('rev-list', ['--all']).count('\n')
-VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
+try:
+    MAJOR_VERSION = '0'
+    MINOR_VERSION = '3'
+    MICRO_VERSION = sh.git('rev-list', ['--all']).count('\n')
+    VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
+    with open('VERSION', 'w') as f: 
+        f.write(VERSION)
+except sh.ErrorReturnCode_128:
+    with open('VERSION') as f: 
+        VERSION = f.read().strip()
 
 setup(name = 'yagmail',
       version = VERSION,
