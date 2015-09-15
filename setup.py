08378228ebcfaf03434f308ad6a818eed355e794
Pascal van Kooten <kootenpv@gmail.com>
2015-09-15 13:33:55 +0200

    added raw and line to the yagmail module level

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 MAJOR_VERSION = '0'
 MINOR_VERSION = '4'
-MICRO_VERSION = '102'
+MICRO_VERSION = '110'
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
 
 setup(name='yagmail',
--- yagmail/__init__.py
+++ yagmail/__init__.py
@@ -1,8 +1,10 @@
 __project__ = 'yagmail'
-__version__ = "0.4.102"
+__version__ = "0.4.110"
 
 from .error import YagConnectionClosed
 from .error import YagAddressError
 from .yagmail import SMTP
 from .yagmail import register
 from .yagmail import logging
+from .yagmail import raw
+from .yagmail import inline
