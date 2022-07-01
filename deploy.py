d0e919f13eaf3b18ed77b956008025757e949318
pascal <pascal@vks.ai>
2022-07-01 13:07:00 +0200

    update deploy script

--- deploy.py
+++ deploy.py
@@ -3,9 +3,9 @@ import re
 import sh
 import os
 
-commit_count = sh.git('rev-list', ['--all']).count('\n')
+commit_count = sh.git("rev-list", ["--all"]).count("\n")
 
-with open('setup.py') as f:
+with open("setup.py") as f:
     setup = f.read()
 
 setup = re.sub("MICRO_VERSION = '[0-9]+'", "MICRO_VERSION = '{}'".format(commit_count), setup)
@@ -13,18 +13,18 @@ setup = re.sub("MICRO_VERSION = '[0-9]+'", "MICRO_VERSION = '{}'".format(commit_
 major = re.search("MAJOR_VERSION = '([0-9]+)'", setup).groups()[0]
 minor = re.search("MINOR_VERSION = '([0-9]+)'", setup).groups()[0]
 micro = re.search("MICRO_VERSION = '([0-9]+)'", setup).groups()[0]
-version = '{}.{}.{}'.format(major, minor, micro)
+version = "{}.{}.{}".format(major, minor, micro)
 
-with open('setup.py', 'w') as f:
+with open("setup.py", "w") as f:
     f.write(setup)
 
-with open('yagmail/__init__.py') as f:
+with open("yagmail/__init__.py") as f:
     init = f.read()
 
-with open('yagmail/__init__.py', 'w') as f:
-    f.write(
-        re.sub('__version__ = "[0-9.]+"',
-               '__version__ = "{}"'.format(version), init))
+with open("yagmail/__init__.py", "w") as f:
+    f.write(re.sub('__version__ = "[0-9.]+"', '__version__ = "{}"'.format(version), init))
 
-py_version = "python3.7" if sh.which("python3.7") is not None else "python"
-os.system('{} setup.py sdist bdist_wheel upload'.format(py_version))
+py_version = "python"
+os.system("rm -rf dist/")
+os.system("{} setup.py sdist bdist_wheel".format(py_version))
+os.system("twine upload dist/*")
